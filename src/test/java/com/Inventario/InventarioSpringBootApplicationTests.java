eb33854b64ea646201316941abc44ba0feba9af9
LilyVaz <vasquezlily75@gmail.com>
2025-02-18 10:29:12 -0600

    Primer actualizacion

---
+++ src/test/java/com/Inventario/InventarioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
